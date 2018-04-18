bef856fc95a3f046316cf8f059b2d87f806633ec
Raksani <raksani.k@ku.th>
2018-04-18 21:06:09 +0700

    add invalid checker method

--- src/Wallet/WalletController.java
+++ src/Wallet/WalletController.java
@@ -5,55 +5,67 @@ import javafx.fxml.FXML;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.TextField;
 
-public class WalletController{
+import java.time.Month;
+import java.time.Year;
+
+public class WalletController {
     @FXML
     private ComboBox categories;
     @FXML
-    private ComboBox date;
+    private ComboBox<Integer> date;
     @FXML
-    private ComboBox month;
+    private ComboBox<Month> month;
     @FXML
-    private ComboBox year;
+    private ComboBox<Integer> year;
     @FXML
     TextField detail;
     @FXML
     TextField amount;
 
     //private list of month.
-    public void initialize () {
-        if(categories!=null) {
-            categories.getItems().addAll("Income","Outcome","Saving" );
+    public void initialize() {
+        if (categories != null) {
+            categories.getItems().addAll("Income", "Outcome", "Saving");
             categories.getSelectionModel().select(0);
         }
-        if(year != null){
+        if (year != null) {
             //I cant remember the range of year we set LOL pls fix it if i'm wrong.
-            for(int i = 2018; i<= 2028; i++){
+            for (int i = 2018; i <= 2028; i++) {
                 year.getItems().add(i);
             }
 
         }
-        if(month != null){
-            month.getItems().addAll("January","Febuary","March","April","May","June","July","August","September","October","November","December");
+        if (month != null) {
+            month.getItems().addAll(Month.values());
             categories.getSelectionModel().select(0);
         }
-        if(date != null){
-            for(int i = 1; i<= 31; i++){
+        if (date != null) {
+            for (int i = 1; i <= 31; i++) {
                 date.getItems().add(i);
             }
         }
     }
 
-    public void handleRecord(ActionEvent event){
-//        //recheck
-//        // categories.getValue().equals?
-//        if(categories.equals("income")){
-//            listIncome.add(date,month,year,detail.getText(),amount.getText());
+    public void InvalidInput(ActionEvent event) {
+        //check date unmatch with year (leap year for february) 2020 2024 2028
+        //check date unmatch with month (30/31 days)
+        if(date.getSelectionModel().getSelectedItem() > month.getSelectionModel().getSelectedItem().length(Year.isLeap(year.getSelectionModel().getSelectedItem()))){
+            //return red border.
+        }
+        //input only number for amount
+    }
+
+    public void handleRecord(ActionEvent event) {
+        //recheck
+        // categories.getValue().equals?
+//        if (categories.equals("income")) {
+//            listIncome.add(date, month, year, detail.getText(), amount.getText());
 //        }
-//        if(categories.equals("outcome")){
-//            listIncome.add(date,month,year,detail.getText(),amount.getText());
+//        if (categories.equals("outcome")) {
+//            listIncome.add(date, month, year, detail.getText(), amount.getText());
 //        }
-//        if(categories.equals("Saving")){
-//            listIncome.add(date,month,year,detail.getText(),amount.getText());
+//        if (categories.equals("Saving")) {
+//            listIncome.add(date, month, year, detail.getText(), amount.getText());
 //        }
     }
 
