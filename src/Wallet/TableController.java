75386e7e5f16608050880f5d7679d4ba521efaeb
Pichaaun Popukdee <pichaaun.p@ku.th>
2018-05-09 20:48:10 +0700

    add remove button

--- src/Wallet/TableController.java
+++ src/Wallet/TableController.java
@@ -48,6 +48,8 @@ public class TableController implements Initializable {
     Button closeButton;
     @FXML
     Button showChart;
+    @FXML
+    Button remove;
 
 
     public static List<String> dateList = new ArrayList<>();
